b0c914bad6559a03e87dedf686a4ab6cc02a52b1
Leila Ybanez <leilanybanez@gmail.com>
2023-07-17 14:19:57 -0300

    feat: implements database methods

--- src/databases/Workout.js
+++ src/databases/Workout.js
@@ -5,6 +5,16 @@ export const getAllWorkouts = () => {
     return db.workouts;
 }
 
+export const getOneWorkout = (workoutId) => {
+    const workout = DB.workouts.find((workout) => workout.id === workoutId);
+
+    if (!workout) {
+        return;
+    }
+
+    return workout;
+}
+
 export const createNewWorkout = (newWorkout) => {
     const isAlreadyAdded = DB.workouts.findIndex((workout) => workout.name === newWorkout.name) > -1; //> /1 && if I try to add the same workout it will return a 201 without the newly inserted workout
     
@@ -15,4 +25,33 @@ export const createNewWorkout = (newWorkout) => {
     DB.workouts.push(newWorkout);
     saveToDatabase(DB);
     return newWorkout;
+}
+
+export const updateOneWorkout = (workoutId, changes) => {
+    const indexForUpdate = DB.workouts.findIndex((workout) => workout.id === workoutId);
+
+    if (indexForUpdate === -1) {
+        return;
+    }
+
+    const updatedWorkout = {
+        ...DB.workouts[indexForUpdate],
+        ...changes,
+        updatedAt: new Date().toLocaleString('en-US', {timeZone: 'utc'})
+    };
+
+    DB.workouts[indexForUpdate] = updatedWorkout;
+    saveToDatabase(DB);
+    return updatedWorkout;
+}
+
+export const deleteOneWorkout = (workoutId) => {
+    const indexForDeletion = DB.workouts.findIndex((workout) => workout.id === workoutId);
+
+    if (indexForDeletion === -1) {
+        return;
+    }
+
+    DB.workouts.splice(indexForDeletion, 1);
+    saveToDatabase(DB);
 }

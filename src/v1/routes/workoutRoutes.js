d7ddfa88e16d4f7ab05bd7c79a1637fd7cd1f8a7
Leila Ybanez <leilanybanez@gmail.com>
2023-10-31 11:11:15 -0300

    chore: replacing

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,25 +1,25 @@
-import express from 'express';
-import 
-    {getAllWorkouts, 
-    getOneWorkout, 
-    createNewWorkout, 
-    updateOneWorkout, 
-    deleteOneWorkout} 
-from '../../controllers/workoutController.js';
-import { getRecordForWorkout } from '../../controllers/recordController.js';
+import express from "express";
+import {
+  getAllWorkouts,
+  getOneWorkout,
+  createNewWorkout,
+  updateOneWorkout,
+  deleteOneWorkout,
+} from "../../controllers/workoutController.js";
+import { getRecordForWorkout } from "../../controllers/recordController.js";
 
 export const router = express.Router();
 
-router.get('/', getAllWorkouts);
+router.get("/", getAllWorkouts);
 
-router.get('/:workoutId', getOneWorkout);
+router.get("/:workoutId", getOneWorkout);
 
-router.get('/', getRecordForWorkout)
+router.get("/:workoutId/records", getRecordForWorkout);
 
-router.post('/', createNewWorkout);
+router.post("/", createNewWorkout);
 
-router.patch('/:workoutId', updateOneWorkout);
+router.patch("/:workoutId", updateOneWorkout);
 
-router.patch('/:workoutId', updateOneWorkout);
+router.patch("/:workoutId", updateOneWorkout);
 
-router.delete('/:workoutId', deleteOneWorkout);
+router.delete("/:workoutId", deleteOneWorkout);

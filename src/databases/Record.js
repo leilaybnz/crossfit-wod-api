83ca6a9ab999394d92a56dd580cbe9c6177fb474
Leila Ybanez <leilanybanez@gmail.com>
2023-11-02 11:09:11 -0300

    feat: create Record database methods

--- src/databases/Record.js
+++ src/databases/Record.js
@@ -1,19 +1,102 @@
-import DB from './db.json' assert {type: 'json'};
+import DB from "./db.json" assert { type: "json" };
+import { saveToDatabase } from "./utils.js";
+
+export const getAllRecords = () => {
+  try {
+    return DB.records;
+  } catch (error) {
+    throw { status: 500, message: error };
+  }
+};
 
 export const getRecordForWorkout = (workoutId) => {
-    try {
+  try {
+    const record = DB.records.filter((record) => record.workout === workoutId); // We filter all the records that are related to the workout id out of the query parameter.
 
-        const record = DB.records.filter((record) => record.workout === workoutId); // We filter all the records that are related to the workout id out of the query parameter.
-
-        if (!record) {
-            throw {
-                status: 400,
-                message: `Cannot find workout with the id '${workoutId}'`
-            };
-        }
-
-        return record;
-    } catch (error) {
-        throw {status: error?.status || 500, message: error?.message || error}
+    if (!record) {
+      throw {
+        status: 400,
+        message: `Cannot find workout with the id '${workoutId}'`,
+      };
     }
-}
+
+    return record;
+  } catch (error) {
+    throw { status: error?.status || 500, message: error?.message || error };
+  }
+};
+
+export const createNewRecord = (newRecord) => {
+  try {
+    DB.records.push(newRecord);
+    saveToDatabase(DB);
+    return newRecord;
+  } catch (error) {
+    throw {
+      status: 500,
+      message: error?.message || error,
+    };
+  }
+};
+
+export const updateOneRecord = (recordId, changes) => {
+  try {
+    const isAlreadyAdded =
+      DB.records.findIndex((record) => record.workout === changes.workout) > -1;
+
+    if (isAlreadyAdded) {
+      throw {
+        status: 400,
+        message: `'Record corresponding to the workout ${changes.workout} already exists.`,
+      };
+    }
+
+    const indexForUpdate = DB.records.findIndex(
+      (record) => record.id === recordId
+    );
+
+    if (indexForUpdate === -1) {
+      throw {
+        status: 400,
+        message: `Cannot find record with the id ${recordId}`,
+      };
+    }
+
+    const updatedRecord = {
+      ...DB.records[indexForUpdate],
+      ...changes,
+      updatedAt: new Date().toLocaleString("es-AR", {
+        timeZone: "America/Buenos_Aires",
+      }),
+    };
+
+    DB.records[indexForUpdate] = updatedRecord;
+    saveToDatabase(DB);
+    return updatedRecord;
+  } catch (error) {
+    throw { status: error?.status || 500, message: error?.message || error };
+  }
+};
+
+export const deleteOneRecord = (recordId) => {
+  try {
+    const indexForDeletion = DB.records.findIndex(
+      (record) => record.id === recordId
+    );
+
+    if (indexForDeletion === -1) {
+      throw {
+        status: 400,
+        message: `Cannot find record with the id: '${recordId}'`,
+      };
+    }
+
+    DB.records.splice(indexForDeletion, 1);
+    saveToDatabase(DB);
+  } catch (error) {
+    throw {
+      status: error?.status || 500,
+      message: error?.message || error,
+    };
+  }
+};

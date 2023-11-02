485594ca137cb5d34c3ff36ca1065168b630973f
Leila Ybanez <leilanybanez@gmail.com>
2023-11-02 11:12:04 -0300

    chore: fix timezone

--- src/services/workoutService.js
+++ src/services/workoutService.js
@@ -1,62 +1,65 @@
-import { v4 as uuid } from 'uuid';
-import {getAllWorkouts, 
-        getOneWorkout, 
-        createNewWorkout, 
-        updateOneWorkout, 
-        deleteOneWorkout} from '../databases/Workout.js';
+import { v4 as uuid } from "uuid";
+import {
+  getAllWorkouts,
+  getOneWorkout,
+  createNewWorkout,
+  updateOneWorkout,
+  deleteOneWorkout,
+} from "../databases/Workout.js";
 
 export const getAllWorkoutsService = () => {
-
-    try {
-       const allWorkouts = getAllWorkouts();
-        return allWorkouts;
-    } catch (error) {
-        throw error;
-    }
-}
+  try {
+    const allWorkouts = getAllWorkouts();
+    return allWorkouts;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export const getOneWorkoutService = (workoutId) => {
-
-    try {
-        const workout = getOneWorkout(workoutId);
-         return workout;
-    } catch (error) {
-         throw error;
-    }
-}
+  try {
+    const workout = getOneWorkout(workoutId);
+    return workout;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export const createNewWorkoutService = (newWorkout) => {
-
-    const workoutToInsert = //will add the remaining properties
-    {...newWorkout, 
-    id: uuid(),
-    createdAt: new Date().toLocaleString('en-US', {timeZone: 'UTC'}),
-    updatedAt: new Date().toLocaleString('en-US', {timeZone: 'utc'})
+  const workoutToInsert =
+    //will add the remaining properties
+    {
+      ...newWorkout,
+      id: uuid(),
+      createdAt: new Date().toLocaleString("es-AR", {
+        timeZone: "America/Buenos_Aires",
+      }),
+      updatedAt: new Date().toLocaleString("es-AR", {
+        timeZone: "America/Buenos_Aires",
+      }),
     };
 
-    try {
-        const createdWorkout = createNewWorkout(workoutToInsert);
-        return createdWorkout;
-    } catch (error) {
-        throw error;
-    }
-
-}
+  try {
+    const createdWorkout = createNewWorkout(workoutToInsert);
+    return createdWorkout;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export const updateOneWorkoutService = (workoutId, changes) => {
-
-    try {
-        const updatedWorkout = updateOneWorkout(workoutId, changes);
-        return updatedWorkout;  
-    } catch (error) {
-        throw error;
-    }
-}
+  try {
+    const updatedWorkout = updateOneWorkout(workoutId, changes);
+    return updatedWorkout;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export const deleteOneWorkoutService = (workoutId) => {
-    try {
-        deleteOneWorkout(workoutId); //why not assign it to a const? why no return?
-    } catch (error) {
-        throw error;
-    }
-}
+  try {
+    deleteOneWorkout(workoutId); //why not assign it to a const? why no return?
+  } catch (error) {
+    throw error;
+  }
+};

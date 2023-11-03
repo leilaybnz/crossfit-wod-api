237885fb06054a1154f2f3a9f8c3f3f7e1d7e276
Leila Ybanez <leilanybanez@gmail.com>
2023-11-03 15:06:44 -0300

    chore: fix typo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {router as v1WorkoutRouer} from './v1/routes/workoutRoutes.js';
+import {router as v1WorkoutRouter} from './v1/routes/workoutRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json())
-app.use('/api/v1/workouts', v1WorkoutRouer)
+app.use('/api/v1/workouts', v1WorkoutRouter)
 
 app.listen(PORT, () => {
     console.log(`API is listening on port ${PORT}`)

0c496aa6ef5aa6d1c2d4f0d057e5f2b8e5e915f8
Leila Ybanez <leilanybanez@gmail.com>
2023-11-03 15:09:48 -0300

    chore: fix order and add Member routes

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -15,24 +15,48 @@ import {
   deleteOneRecordController,
 } from "../../controllers/recordController.js";
 
+import {
+  getAllMembersController,
+  getMemberForRecordController,
+  createNewMemberController,
+  updateMemberController,
+  deleteMemberController
+} from '../../controllers/memberController.js';
+
 export const router = express.Router();
+
+//WORKOUT ROUTES
 
 router.get("/", getAllWorkoutsController);
 
-router.get("/records", getAllRecordsController);
-
 router.get("/:workoutId", getOneWorkoutController);
-
-router.get("/:workoutId/records", getRecordForWorkoutController);
 
 router.post("/", createNewWorkoutController);
 
-router.post("/:workoutId/records", createNewRecordController);
-
 router.patch("/:workoutId", updateOneWorkoutController);
-
-router.patch("/:workoutId/records/:recordId", updateOneRecordController);
 
 router.delete("/:workoutId", deleteOneWorkoutController);
 
+//RECORDS ROUTES
+
+router.get("/", getAllRecordsController);
+
+router.get("/:workoutId/records", getRecordForWorkoutController);
+
+router.post("/:workoutId/records", createNewRecordController);
+
+router.patch("/:workoutId/records/:recordId", updateOneRecordController);
+
 router.delete("/:workoutId/records/:recordId", deleteOneRecordController);//THROWS 200 BUT DOESNT DELETE
+
+//MEMBERS ROUTES
+
+router.get("/", getAllMembersController);
+
+router.get("/:workoutId/records/:recordId/member", getMemberForRecordController);
+
+router.post("/members/:memberId", createNewMemberController);
+
+router.patch("/members/:memberId", updateMemberController);
+
+router.delete("/members/:memberId", deleteMemberController);

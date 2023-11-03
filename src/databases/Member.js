d512a8492ac0dc87d2a221e7c4f0ebf1ec53d41f
Leila Ybanez <leilanybanez@gmail.com>
2023-11-03 15:09:18 -0300

    feat: start Member logic

---
+++ src/databases/Member.js
@@ -0,0 +1,96 @@
+import DB from './db.json' assert {type: 'json'};
+import { saveToDatabase } from './utils.js';
+
+export const getAllMembers = () => {
+
+    try {
+        return DB.members;
+    } catch (error) {
+        throw {status: 500, message: error}
+    }
+
+}
+
+export const getMemberForRecord = (recordId) => {
+    try {
+      const record = DB.records.filter((record) => record.id === recordId); // We filter all the records that are related to the workout id out of the query parameter.
+  
+      if (!record) {
+        throw {
+          status: 400,
+          message: `Cannot find record with the id '${recordId}'`,
+        };
+      }
+  
+      return record;
+    } catch (error) {
+      throw { status: error?.status || 500, message: error?.message || error };
+    }
+  };
+
+export const createNewMember = (newMember) => {
+    const isAlreadyAdded = DB.members.findIndex((member) => member.name === newMember.name) > -1; //> /1 && if I try to add the same workout it will return a 201 without the newly inserted workout
+    
+    if (isAlreadyAdded) {
+        throw {
+            status: 400,
+            message: `Member with the name '${newMember.name}' already exists.`
+        };
+    }
+
+    try {
+        DB.members.push(newMember);
+        saveToDatabase(DB);
+        return newMember;
+    } catch (error) {
+        throw {
+            status: 500,
+            message: error?.message || error
+        }; 
+    }
+
+}
+
+export const updateMember = (memberId, changes) => {
+
+    try {
+        const indexForUpdate = DB.members.findIndex((member) => member.id === memberId);
+    
+        if (indexForUpdate === -1) {
+            throw {
+                status: 400,
+                message: `Cannot find member with the id '${memberId}'`
+            }
+        }
+    
+        const updatedMember = {
+            ...DB.members[indexForUpdate],
+            ...changes,
+            updatedAt: new Date().toLocaleString('es-AR', {timeZone: 'America/Buenos_Aires'})
+        };
+    
+        DB.members[indexForUpdate] = updatedMember;
+        saveToDatabase(DB);
+         return updatedMember;
+    } catch (error) {
+        throw {status: error?.status || 500, message: error?.message || error};
+    }
+}
+
+export const deleteMember = (memberId) => {
+    try {
+        const indexForDeletion = DB.members.findIndex((member) => member.id === memberId);
+        
+        if (indexForDeletion === -1) {
+            throw {
+                status: 400,
+                message: `Cannot find member with the id '${memberId}'`
+            }
+        }
+        
+        DB.members.splice(indexForDeletion, 1);
+        saveToDatabase(DB);
+    } catch (error) {
+        throw {status: error?.status || 500, message: error?.message || error}
+    } 
+}
---
+++ src/services/memberService.js
@@ -0,0 +1,67 @@
+import { v4 as uuid } from "uuid";
+import {
+    getAllMembers,
+    getMemberForRecord,
+    createNewMember,
+    updateMember,
+    deleteMember
+} from '../databases/Member.js';
+
+export const getAllMembersService = () => {
+  try {
+    const allMembers = getAllMembers();
+    return allMembers;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getMemberForRecordService = (recordId) => {
+
+    try {
+       const member = getMemberForRecord(recordId);
+        return member;
+    } catch (error) {
+        throw error;
+    }
+
+}
+
+export const createNewMemberService = (newMember) => {
+  const memberToInsert =
+    //will add the remaining properties
+    {
+      ...newMember,
+      id: uuid(),
+      createdAt: new Date().toLocaleString("es-AR", {
+        timeZone: "America/Buenos_Aires",
+      }),
+      updatedAt: new Date().toLocaleString("es-AR", {
+        timeZone: "America/Buenos_Aires",
+      }),
+    };
+
+  try {
+    const createdMember = createNewMember(memberToInsert);
+    return createdMember;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const updateMemberService = (memberId, changes) => {
+  try {
+    const updatedMember = updateMember(memberId, changes);
+    return updatedMember;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteMemberService = (memberId) => {
+  try {
+    deleteMember(memberId); //why not assign it to a const? why no return?
+  } catch (error) {
+    throw error;
+  }
+};

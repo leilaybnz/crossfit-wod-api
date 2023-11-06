e12b09afff60f96168b621e57eac9803731ed128
Leila Ybanez <leilanybanez@gmail.com>
2023-11-06 14:15:27 -0300

    chore: comment

--- src/databases/Member.js
+++ src/databases/Member.js
@@ -29,7 +29,7 @@ export const getMemberForRecord = (recordId) => {
   };
 
 export const createNewMember = (newMember) => {
-    const isAlreadyAdded = DB.members.findIndex((member) => member.name === newMember.name) > -1; //> /1 && if I try to add the same workout it will return a 201 without the newly inserted workout
+    const isAlreadyAdded = DB.members.findIndex((member) => member.name === newMember.name) > -1; //> /1 && if I try to add the same member it will return a 201 without the newly inserted member
     
     if (isAlreadyAdded) {
         throw {

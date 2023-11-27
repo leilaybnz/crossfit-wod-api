f0f692c42bd341f70cee2af15f695e5cd1bec5f5
Leila Ybanez <leilanybanez@gmail.com>
2023-11-27 11:08:37 -0300

    feat: allow CORS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from "cors";
 import {router as v1WorkoutRouter} from './v1/routes/workoutRoutes.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
+const allowedOrigins = ['http://localhost:3000'];
+
+const options = {
+  origin: allowedOrigins
+};
+
+app.use(cors(options));
 app.use(bodyParser.json())
 app.use('/api/v1/workouts', v1WorkoutRouter)
 

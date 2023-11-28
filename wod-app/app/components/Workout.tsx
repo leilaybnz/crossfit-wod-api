eab2b04f4c541dc146fe9d25c31c850976b78d2b
Leila Ybanez <leilanybanez@gmail.com>
2023-11-28 14:09:47 -0300

    feat: workout component styles

--- wod-app/app/components/Workout.tsx
+++ wod-app/app/components/Workout.tsx
@@ -1,26 +1,26 @@
 import styles from "../styles/workout.module.css";
 import { WorkoutType } from "../types";
   
-export default function Workout(workout: WorkoutType) {
+export default function Workout({ workout }: {workout: WorkoutType}) {
 
   return (
-       <div className={styles.container}>
+      <article className={styles.container}>
          <p className={styles.title}>Workout</p>
          <p className={styles.name}>
            Name: <span className={styles.text}>{workout.name}</span>
          </p>
          <p className={styles.name}>
-           Mode: <span className={styles.text}>WM</span>
+           Mode: <span className={styles.text}>{workout.mode}</span>
          </p>
          <p className={styles.name}>
-           Equipment: <span className={styles.text}>WE</span>
+           Equipment: <span className={styles.text}>{workout.equipment}</span>
          </p>
          <p className={styles.name}>
-           Exercises: <span className={styles.text}>WEX</span>
+           Exercises: <span className={styles.text}>{workout.exercises}</span>
          </p>
          <p className={styles.name}>
-           Trainer tips: <span className={styles.text}>WTT</span>
+           Trainer tips: <span className={styles.text}>{workout.trainerTips}</span>
          </p>
-       </div>
+       </article>
   )
 }

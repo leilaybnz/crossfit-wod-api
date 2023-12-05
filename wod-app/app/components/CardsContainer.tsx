6724779b9699369ee757930cb7e1af7a6d6d3c77
Leila Ybanez <leilanybanez@gmail.com>
2023-12-05 12:18:16 -0300

    formatting

--- wod-app/app/components/CardsContainer.tsx
+++ wod-app/app/components/CardsContainer.tsx
@@ -1,38 +1,37 @@
-'use client'
+"use client";
 import Workout from "./Workout";
 import Member from "./Member";
-import styles from '../styles/cardsContainer.module.css';
+import styles from "../styles/cardsContainer.module.css";
 import { useEffect, useState } from "react";
 import { WorkoutType } from "../types";
 
 export default function CardsContainer() {
+  const [workouts, setWorkouts] = useState<WorkoutType[]>([]);
 
-    const [workouts, setWorkouts] = useState<WorkoutType[]>([]);
+  useEffect(() => {
+    fetch(`http://localhost:5000/api/v1/workouts`)
+      .then((response) => response.json())
+      .then((json) => {
+        const data = json.allWorkouts;
+        setWorkouts(data);
+      });
+  }, []);
 
-     useEffect(() => {
-             fetch(`http://localhost:5000/api/v1/workouts`)
-             .then((response) => response.json())
-             .then((json) => {
-             const data = json.allWorkouts;
-             setWorkouts(data);
-             })
-     }, []);
-
-    return(
-        <>
-        {/* <pre>{JSON.stringify(workouts, null, 2)}</pre> */}
-        <section className={styles.container}>
+  return (
+    <>
+      {/* <pre>{JSON.stringify(workouts, null, 2)}</pre> */}
+      <section className={styles.container}>
         {workouts.map((workout, i) => (
-            <Workout workout={workout} key={i}/>
+          <Workout workout={workout} key={i} />
         ))}
-        </section>
-        <section className={styles.container}>
-            <Member/>
-            <Member/>
-            <Member/>
-            <Member/>
-            <Member/>
-        </section>
-        </>
-    )
+      </section>
+      <section className={styles.container}>
+        <Member />
+        <Member />
+        <Member />
+        <Member />
+        <Member />
+      </section>
+    </>
+  );
 }

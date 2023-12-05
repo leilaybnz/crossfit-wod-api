6a8ec9d6c5dc9677e12bac64d6827bcf43a02aa8
Leila Ybanez <leilanybanez@gmail.com>
2023-12-05 12:18:56 -0300

    feat: create AddRemoveInput component

---
+++ wod-app/app/components/AddRemoveInput.tsx
@@ -0,0 +1,42 @@
+import { ChangeEvent, useState } from "react";
+import styles from "../styles/addRemoveInput.module.css";
+
+function AddRemoveInputField() {
+  const [inputFields, setInputFields] = useState([{ equipment: "" }]);
+
+  const handleFormChange = (
+    index: any,
+    event: ChangeEvent<HTMLInputElement>
+  ) => {
+    let data = [...inputFields];
+    data[index][event.target.name] = event.target.value;
+  };
+
+  const addInput = () => {
+    let newField = { equipment: "" };
+    setInputFields([...inputFields, newField]);
+  };
+
+  return (
+    <div className={styles.form}>
+      {inputFields.map((input, index) => {
+        return (
+          <>
+            <input
+              name="equipment"
+              placeholder="Add workout equipment"
+              value={input.equipment}
+              onChange={() => handleFormChange(index, event)}
+              className={styles.input}
+              key={index}
+            />
+            <button onClick={addInput} className={styles.button}>
+              +
+            </button>
+          </>
+        );
+      })}
+    </div>
+  );
+}
+export default AddRemoveInputField;

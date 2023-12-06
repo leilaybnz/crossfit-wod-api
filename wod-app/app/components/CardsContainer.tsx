f67c3e84b87a05caba6ebac92c25b4f4c4f510cf
Leila Ybanez <leilanybanez@gmail.com>
2023-12-06 17:44:14 -0300

    chore: show buttons instead of workout cards. WIP

--- wod-app/app/components/CardsContainer.tsx
+++ wod-app/app/components/CardsContainer.tsx
@@ -4,9 +4,11 @@ import Member from "./Member";
 import styles from "../styles/cardsContainer.module.css";
 import { useEffect, useState } from "react";
 import { WorkoutType } from "../types";
+import Button from "./Button";
 
 export default function CardsContainer() {
   const [workouts, setWorkouts] = useState<WorkoutType[]>([]);
+  const [isShown, setIsShown] = useState(false);
 
   useEffect(() => {
     fetch(`http://localhost:5000/api/v1/workouts`)
@@ -17,21 +19,31 @@ export default function CardsContainer() {
       });
   }, []);
 
+  const handleClick = () => {
+    console.log("click");
+    setIsShown(true);
+  };
+
   return (
     <>
-      {/* <pre>{JSON.stringify(workouts, null, 2)}</pre> */}
-      <section className={styles.container}>
-        {workouts.map((workout, i) => (
-          <Workout workout={workout} key={i} />
-        ))}
-      </section>
+      {/* <pre>{JSON.stringify(workouts, null, 2)}</pre>
+     
       <section className={styles.container}>
         <Member />
         <Member />
         <Member />
         <Member />
         <Member />
-      </section>
+      </section> */}
+      <Button title="workouts" handleClick={handleClick} />
+      <Button title="members" />
+      {isShown && (
+        <section className={styles.container}>
+          {workouts.map((workout, i) => (
+            <Workout workout={workout} key={i} />
+          ))}
+        </section>
+      )}
     </>
   );
 }

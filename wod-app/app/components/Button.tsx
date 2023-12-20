8598eb46bc35a247efc224d25fb705c255c8f043
Leila Ybanez <leilanybanez@gmail.com>
2023-12-20 11:44:45 -0300

    feat: pass onClick with props and remove all logic

--- wod-app/app/components/Button.tsx
+++ wod-app/app/components/Button.tsx
@@ -1,19 +1,11 @@
-import { useState } from "react";
 import styles from "../styles/button.module.css";
 
 export default function Button(props: any) {
-  const [isShown, setIsShown] = useState(false);
-
-  const handleClick = () => {
-    setIsShown(true);
-  }
-
   return (
     <>
-      <button className={styles.button}>
+      <button className={styles.button} onClick={props.onClick}>
         See all {props.title}
       </button>
-      {isShown && <p>isShown</p>}
     </>
   );
 }

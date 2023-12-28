68d9874d331ad25bf05d10abbfbbb572e5570c57
Leila Ybanez <leilanybanez@gmail.com>
2023-12-28 11:21:28 -0300

    chore: remove input logic

--- wod-app/app/components/AddRemoveInput.tsx
+++ wod-app/app/components/AddRemoveInput.tsx
@@ -2,37 +2,57 @@ import { ChangeEvent, useState } from "react";
 import styles from "../styles/addRemoveInput.module.css";
 
 export default function AddRemoveInputField() {
-  const [inputFields, setInputFields] = useState([{ equipment: "" }]);
+  const [inputList, setInputList] = useState([{ equipment: "" }]);
 
-  const handleFormChange = (
+  const handleInputChange = (
     index: any,
     event: ChangeEvent<HTMLInputElement>
   ) => {
-    let data = [...inputFields];
-    data[index][event.target.name] = event.target.value;
+    const { name, value } = event.target;
+    let fields = [...inputList];
+    fields[index][name] = value;
+    setInputList(fields);
   };
 
   const addInput = () => {
-    let newField = { equipment: "" };
-    setInputFields([...inputFields, newField]);
+    let newInput = { equipment: "" };
+    setInputList([...inputList, newInput]);
+  };
+  const removeInput = (index: any) => {
+    const list = [...inputList];
+    list.splice(index, 1);
+    setInputList(list);
   };
 
   return (
     <div className={styles.form}>
-      {inputFields.map((input, index) => {
+      {inputList.map((input, index) => {
         return (
           <>
-            <input
-              name="equipment"
-              placeholder="Add workout equipment"
-              value={input.equipment}
-              onChange={() => handleFormChange(index, event)}
-              className={styles.input}
-              key={index}
-            />
-            <button onClick={addInput} className={styles.button}>
-              +
-            </button>
+            <div className={styles.inputContainer}>
+              <input
+                name="equipment"
+                placeholder="Add workout equipment"
+                value={input.equipment}
+                onChange={() => handleInputChange(index, event)}
+                className={styles.input}
+                key={index}
+              />
+            </div>
+            <div className={styles.buttonContainer}>
+              <button
+                onClick={addInput}
+                className={`${styles.button} ${styles.add}`}
+              >
+                +
+              </button>
+              <button
+                onClick={removeInput}
+                className={`${styles.button} ${styles.remove}`}
+              >
+                -
+              </button>
+            </div>
           </>
         );
       })}

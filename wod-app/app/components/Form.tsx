79bd6a6c0a4141779105b894c37a5aabf6c4a88a
Leila Ybanez <leilanybanez@gmail.com>
2024-01-03 10:28:22 -0300

    feat: create PostWorkout interface and move fetch code to postWorkout function

--- wod-app/app/components/Form.tsx
+++ wod-app/app/components/Form.tsx
@@ -2,6 +2,24 @@ import { FormEvent, useState } from "react";
 import styles from "../styles/form.module.css";
 import AddRemoveInputField from "./AddRemoveInput";
 
+interface PostWorkoutProps {
+  name: string;
+  mode: string;
+  equipment: string[];
+  exercises: string[];
+  trainerTips: string[];
+}
+
+function postWorkout(body: PostWorkoutProps) {
+  return fetch("http://localhost:5000/api/v1/workouts", {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  }).then((response) => response.json());
+}
+
 export default function Form() {
   const [newName, setNewName] = useState("");
   const [newMode, setNewMode] = useState("");
@@ -11,80 +29,96 @@ export default function Form() {
 
   const addWorkout = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const name = newName.trim()
-    const mode = newMode.trim()
+    const name = newName.trim();
+    const mode = newMode.trim();
     const equipment = newEquipment;
     const exercises = newExercises;
     const trainerTips = newTrainerTips;
 
     if (name && mode && equipment && exercises && trainerTips) {
-      fetch("http://localhost:5000/api/v1/workouts", {
-        method: "POST",
-        body: JSON.stringify({
-          name,
-          mode,
-          equipment,
-          exercises,
-          trainerTips
-        }),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-        },
-      })
-        .then(response => response.json())
-        .then(data => {
-          setNewName("")
-          setNewMode("")
-          setNewEquipment([])
-          setNewExercises([])
-          setNewTrainerTips([])
-        })
+      postWorkout({
+        name,
+        mode,
+        equipment,
+        exercises,
+        trainerTips,
+      }).then((data) => {
+        setNewName("");
+        setNewMode("");
+        setNewEquipment([]);
+        setNewExercises([]);
+        setNewTrainerTips([]);
+      });
     }
-  }
+  };
 
   return (
-      <form className={styles.form} onClick={addWorkout}>
-        <label className={styles.label}>
-          Name
-          <input
-            type="text"
-            name="name"
-            placeholder="Add workout name"
-            aria-required="true"
-            value={newName}
-            onChange={(e) => setNewName(e.target.value)}
-          />
-        </label>
-        <label className={styles.label}>
-          Mode
-          <input
-            type="text"
-            name="mode"
-            placeholder="Add workout mode"
-            aria-required="true"
-            value={newMode}
-            onChange={(e) => setNewMode(e.target.value)}
-          />
-        </label>
-        <label className={styles.label}>
-          Equipment
-          <AddRemoveInputField />
-        </label>
-        <label className={styles.label}>
-          Exercises
-          <input name="exercises" placeholder="Add workout exercises" aria-required="true" />
-          <input name="exercises" placeholder="Add workout exercises" aria-required="true" />
-          <input name="exercises" placeholder="Add workout exercises" aria-required="true" />
-        </label>
-        <label className={styles.label}>
-          Trainer tips
-          <input name="trainer tips" placeholder="Add workout trainer tips" aria-required="true" />
-          <input name="trainer tips" placeholder="Add workout trainer tips" aria-required="true" />
-          <input name="trainer tips" placeholder="Add workout trainer tips" aria-required="true" />
-        </label>
-        <button className={styles.button} type="submit">
-          Submit
-        </button>
-      </form>
-  )
+    <form className={styles.form} onClick={addWorkout}>
+      <label className={styles.label}>
+        Name
+        <input
+          type="text"
+          name="name"
+          placeholder="Add workout name"
+          aria-required="true"
+          value={newName}
+          onChange={(e) => setNewName(e.target.value)}
+        />
+      </label>
+      <label className={styles.label}>
+        Mode
+        <input
+          type="text"
+          name="mode"
+          placeholder="Add workout mode"
+          aria-required="true"
+          value={newMode}
+          onChange={(e) => setNewMode(e.target.value)}
+        />
+      </label>
+      <label className={styles.label}>
+        Equipment
+        <AddRemoveInputField />
+      </label>
+      <label className={styles.label}>
+        Exercises
+        <input
+          name="exercises"
+          placeholder="Add workout exercises"
+          aria-required="true"
+        />
+        <input
+          name="exercises"
+          placeholder="Add workout exercises"
+          aria-required="true"
+        />
+        <input
+          name="exercises"
+          placeholder="Add workout exercises"
+          aria-required="true"
+        />
+      </label>
+      <label className={styles.label}>
+        Trainer tips
+        <input
+          name="trainer tips"
+          placeholder="Add workout trainer tips"
+          aria-required="true"
+        />
+        <input
+          name="trainer tips"
+          placeholder="Add workout trainer tips"
+          aria-required="true"
+        />
+        <input
+          name="trainer tips"
+          placeholder="Add workout trainer tips"
+          aria-required="true"
+        />
+      </label>
+      <button className={styles.button} type="submit">
+        Submit
+      </button>
+    </form>
+  );
 }

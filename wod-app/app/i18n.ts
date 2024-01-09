2c6d89a205de2033b300d40e87a1b0588f22c5af
Leila Ybanez <leilanybanez@gmail.com>
2024-01-09 17:46:04 -0300

    feat: import language detector

--- wod-app/app/i18n.ts
+++ wod-app/app/i18n.ts
@@ -1,12 +1,19 @@
 import i18n from "i18next";
 import { useTranslation, initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import esJSON from "./locale/es.json";
 import enJSON from "./locale/en.json";
+import i18next from "i18next";
 
-i18n.use(initReactI18next).init({
-  resources: {
-    es: { ...esJSON },
-    en: { ...enJSON },
-  },
-  lng: "en",
-});
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      es: { ...esJSON },
+      en: { ...enJSON },
+    },
+    lng: "en",
+  });
+
+  export default i18n;

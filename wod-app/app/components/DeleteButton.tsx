c1f55f4ae79ef278a6cfda9fa1b1aa1c45fc5398
Leila Ybanez <leilanybanez@gmail.com>
2024-01-10 16:15:47 -0300

    feat: add confirmation modal

--- wod-app/app/components/DeleteButton.tsx
+++ wod-app/app/components/DeleteButton.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "../styles/deleteButton.module.css";
 import TrashCanSVG from "./TrashCanSvg";
 
@@ -12,13 +13,26 @@ interface DeleteButtonProps {
 }
 
 export default function DeleteButton({ workoutId }: DeleteButtonProps) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <button
-      type="button"
-      className={styles.container}
-      onClick={() => deleteWorkout(workoutId)}
-    >
-      <TrashCanSVG />
-    </button>
+    <>
+      <button
+        type="button"
+        className={styles.container}
+        onClick={() => setIsModalOpen(!isModalOpen)}
+      >
+        <TrashCanSVG />
+      </button>
+      {isModalOpen && (
+        <div className={styles.modal}>
+          <p>Are you sure you want to delete this workout?</p>
+          <div className={styles.buttonContainer}>
+            <button type="button" onClick={() => deleteWorkout(workoutId)}>Yes</button>
+            <button type="button" onClick={() => setIsModalOpen(false)}>No</button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }

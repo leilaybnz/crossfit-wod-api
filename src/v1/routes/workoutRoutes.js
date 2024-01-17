dfb2b740113d557d349ed3a83dbe4f32a4bdbad7
Leila Ybanez <leilanybanez@gmail.com>
2024-01-17 13:46:36 -0300

    feat: group and clear cache fn

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -21,23 +21,55 @@ import {
   getMemberForRecordController,
   createNewMemberController,
   updateMemberController,
-  deleteMemberController
-} from '../../controllers/memberController.js';
+  deleteMemberController,
+} from "../../controllers/memberController.js";
 
 export const router = express.Router();
 const cache = apicache.middleware;
 
+function groupCacheBy(key) {
+  return (req, res, next) => {
+    req.apicacheGroup = "workouts";
+    next();
+  };
+}
+
+function clearCacheKey(key) {
+  return (req, res, next) => {
+    apicache.clear(key);
+    next();
+  };
+}
+
+const workoutsCache = {
+  addToGroup: groupCacheBy("workouts"),
+  clearGroup: clearCacheKey("workouts"),
+};
+
 //WORKOUT ROUTES
 
-router.get("/", cache("2 minutes"), getAllWorkoutsController);
+router.get(
+  "/",
+  cache("30 seconds"),
+  workoutsCache.addToGroup,
+  getAllWorkoutsController
+);
 
 router.get("/:workoutId", getOneWorkoutController);
 
-router.post("/", createNewWorkoutController);
+router.post("/", workoutsCache.clearGroup, createNewWorkoutController);
 
-router.patch("/:workoutId", updateOneWorkoutController);
+router.patch(
+  "/:workoutId",
+  workoutsCache.clearGroup,
+  updateOneWorkoutController
+);
 
-router.delete("/:workoutId", deleteOneWorkoutController);
+router.delete(
+  "/:workoutId",
+  workoutsCache.clearGroup,
+  deleteOneWorkoutController
+);
 
 //RECORDS ROUTES
 
@@ -49,13 +81,16 @@ router.post("/:workoutId/records", createNewRecordController);
 
 router.patch("/:workoutId/records/:recordId", updateOneRecordController);
 
-router.delete("/:workoutId/records/:recordId", deleteOneRecordController);//THROWS 200 BUT DOESNT DELETE
+router.delete("/:workoutId/records/:recordId", deleteOneRecordController); //THROWS 200 BUT DOESNT DELETE
 
 //MEMBERS ROUTES
 
 router.get("/", getAllMembersController); //Cannot GET /api/v1/members/
 
-router.get("/:workoutId/records/:recordId/member", getMemberForRecordController);
+router.get(
+  "/:workoutId/records/:recordId/member",
+  getMemberForRecordController
+);
 
 router.post("/members/:memberId", createNewMemberController);
 

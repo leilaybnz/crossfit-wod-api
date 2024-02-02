747d4a24c3ea5e40e8f3a9f2ec2fa0cedfe52206
Leila Ybanez <leilanybanez@gmail.com>
2024-02-02 17:48:16 -0300

    chore: set all rules in a single card css file

--- wod-app/app/components/Workout.tsx
+++ wod-app/app/components/Workout.tsx
@@ -1,5 +1,5 @@
 import { Dispatch, SetStateAction } from "react";
-import styles from "../styles/workout.module.css";
+import styles from "../styles/card.module.css";
 import { WorkoutType } from "../types";
 import DeleteWorkoutButton from "./DeleteWorkoutButton";
 
@@ -10,7 +10,7 @@ interface WorkoutProps{
 
 export default function Workout({ workout, setShouldRefresh }: WorkoutProps) {
   return (
-    <article className={styles.container}>
+    <article className={styles.containerWorkout}>
       <p className={styles.title}>Workout</p>
       <p className={styles.name}>
         Name: <span className={styles.text}>{workout.name}</span>

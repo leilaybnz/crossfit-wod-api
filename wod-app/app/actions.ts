ee1e54d0fc783ef35084497539f997f919d8d004
Leila Ybanez <leilanybanez@gmail.com>
2024-02-10 20:19:25 -0300

    chore: wip createNew actions

--- wod-app/app/actions.ts
+++ wod-app/app/actions.ts
@@ -1,14 +1,29 @@
-"use server"
+"use server";
 
-import { revalidatePath } from "next/cache"
-import { deleteMember, deleteWorkout } from "./api/wod";
+import { revalidatePath } from "next/cache";
+import {
+  deleteWorkout,
+  deleteMember,
+  createNewWorkout,
+  createNewMember,
+} from "./services/wod";
 
 export async function deleteWorkoutAction(workoutId: string) {
-    revalidatePath("/");
-    return deleteWorkout(workoutId);
+  revalidatePath("/");
+  return deleteWorkout(workoutId);
 }
 
-export async function deleteMemberAction(memberId:string) {
-    revalidatePath("/");
-    return deleteMember(memberId);
+export async function deleteMemberAction(memberId: string) {
+  revalidatePath("/");
+  return deleteMember(memberId);
+}
+
+export async function createWorkoutAction() {
+  revalidatePath("/");
+  return createNewWorkout(newWorkout);
+}
+
+export async function createMemberAction() {
+  revalidatePath("/");
+  return createNewMember(newMember);
 }

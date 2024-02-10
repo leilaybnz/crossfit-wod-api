2f5604e3237381567fd53ac02950ae4aef5f9f35
Leila Ybanez <leilanybanez@gmail.com>
2024-02-10 20:20:31 -0300

    chore: add postMember route

--- wod-app/app/api/route.ts
+++ wod-app/app/api/route.ts
@@ -1,9 +1,13 @@
 import { randomUUID } from "crypto";
 import { revalidatePath } from "next/cache";
 import { NextRequest, NextResponse } from "next/server";
-import { AlreadyExistsError, createNewWorkout } from "../../services/wod";
+import {
+  AlreadyExistsError,
+  createNewMember,
+  createNewWorkout,
+} from "../services/wod";
 
-export async function POST(request: NextRequest) {
+export async function postWorkout(request: NextRequest) {
   const body = await request.json();
 
   if (
@@ -55,6 +59,86 @@ export async function POST(request: NextRequest) {
       {
         status: "OK",
         data: createdWorkout,
+      },
+      {
+        status: 201,
+      }
+    );
+  } catch (error: unknown) {
+    if (error instanceof AlreadyExistsError) {
+      return NextResponse.json(
+        {
+          status: "FAILED",
+          data: { error: error.message },
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        status: "FAILED",
+        data: { error },
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
+
+export async function postMember(request: NextRequest) {
+  const body = await request.json();
+
+  if (
+    !body.name ||
+    !body.gender ||
+    !body.dateOfBirth ||
+    !body.email ||
+    !body.password ||
+    !body.member
+  ) {
+    return NextResponse.json(
+      {
+        status: "FAILED",
+        data: {
+          error:
+            "One of the following keys is missing or is empty in request body: 'name', 'gender', 'dateOfBirth', 'email', 'password', 'member'",
+        },
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  const newMember = {
+    name: body.name,
+    gender: body.gender,
+    dateOfBirth: body.dateOfBirth,
+    email: body.email,
+    password: body.password,
+    member: body.member,
+    id: randomUUID(),
+    createdAt: new Date().toLocaleString("es-AR", {
+      timeZone: "America/Buenos_Aires",
+    }),
+    updatedAt: new Date().toLocaleString("es-AR", {
+      timeZone: "America/Buenos_Aires",
+    }),
+  };
+
+  revalidatePath("/");
+
+  try {
+    const createdMember = await createNewMember(newMember);
+
+    return NextResponse.json(
+      {
+        status: "OK",
+        data: createdMember,
       },
       {
         status: 201,

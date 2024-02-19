7fc829ce73e05a73f0a7331f9b30d3b1201cf16f
Leila Ybanez <leilanybanez@gmail.com>
2024-02-19 10:14:24 -0300

    chore: fix array types

--- wod-app/app/types.d.ts
+++ wod-app/app/types.d.ts
@@ -3,11 +3,11 @@ import { Dispatch, SetStateAction } from "react";
 export interface WorkoutType {
   name: string;
   mode: string;
-  equipment: string;
-  mobility: string,
-  activation: string,
-  exercises: string;
-  trainerTips: string;
+  equipment: string[];
+  mobility: string[];
+  activation: string[];
+  exercises: string[];
+  trainerTips: string[];
   id: string;
 }
 

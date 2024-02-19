e1e1d979fd898d75e0240aa283347c19e961f733
Leila Ybanez <leilanybanez@gmail.com>
2024-02-19 13:15:14 -0300

    chore: optional properties

--- wod-app/app/types.d.ts
+++ wod-app/app/types.d.ts
@@ -4,8 +4,8 @@ export interface WorkoutType {
   name: string;
   mode: string;
   equipment: string[];
-  mobility: string[];
-  activation: string[];
+  mobility?: string[];
+  activation?: string[];
   exercises: string[];
   trainerTips: string[];
   id: string;

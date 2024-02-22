275e5ece2db4f74801e2952bfe9b5166a6c9bf7b
Leila Ybanez <leilanybanez@gmail.com>
2024-02-21 23:16:39 -0300

    chore: trainerTips optional show

--- wod-app/app/components/Workout.tsx
+++ wod-app/app/components/Workout.tsx
@@ -11,56 +11,57 @@ export default function Workout({ workout }: WorkoutProps) {
   return (
     <article className={styles.containerWorkout}>
       <p className={styles.title}>Workout</p>
-        <p className={styles.name}>
-          Name: <span className={styles.text}>{workout.name}</span>
-        </p>
-        <p className={styles.name}>
-          Mode: <span className={styles.text}>{workout.mode}</span>
-        </p>
+      <p className={styles.name}>
+        Name: <span className={styles.text}>{workout.name}</span>
+      </p>
+      <p className={styles.name}>
+        Mode: <span className={styles.text}>{workout.mode}</span>
+      </p>
+      <div className={styles.name}>
+        Equipment:
+        <ul className={styles.list}>
+          {workout.equipment.map((equipment) => (
+            <li key={equipment} className={styles.text}>
+              {" " + equipment}
+            </li>
+          ))}
+        </ul>
+      </div>
+      {workout.mobility && (
         <div className={styles.name}>
-          Equipment:
+          Mobility:
           <ul className={styles.list}>
-            {workout.equipment.map((equipment) => (
-              <li key={equipment} className={styles.text}>
-                {" " + equipment}
+            {workout.mobility.map((mobility) => (
+              <li key={mobility} className={styles.text}>
+                {" " + mobility}
               </li>
             ))}
           </ul>
         </div>
-        {workout.mobility && (
-          <div className={styles.name}>
-            Mobility:
-            <ul className={styles.list}>
-              {workout.mobility.map((mobility) => (
-                <li key={mobility} className={styles.text}>
-                  {" " + mobility}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-        {workout.activation && (
-          <div className={styles.name}>
-            Activation:
-            <ul className={styles.list}>
-              {workout.activation.map((activation) => (
-                <li key={activation} className={styles.text}>
-                  {" " + activation}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
+      )}
+      {workout.activation && (
         <div className={styles.name}>
-          Exercises:
-          <ul className={styles.listThunder}>
-            {workout.exercises.map((exercise) => (
-              <li key={exercise} className={styles.text}>
-                {" " + exercise}
+          Activation:
+          <ul className={styles.list}>
+            {workout.activation.map((activation) => (
+              <li key={activation} className={styles.text}>
+                {" " + activation}
               </li>
             ))}
           </ul>
         </div>
+      )}
+      <div className={styles.name}>
+        Exercises:
+        <ul className={styles.listThunder}>
+          {workout.exercises.map((exercise) => (
+            <li key={exercise} className={styles.text}>
+              {" " + exercise}
+            </li>
+          ))}
+        </ul>
+      </div>
+      {workout.trainerTips && (
         <div className={styles.name}>
           Trainer tips:
           <ul className={styles.list}>
@@ -71,6 +72,7 @@ export default function Workout({ workout }: WorkoutProps) {
             ))}
           </ul>
         </div>
+      )}
       <div className={styles.arrow}></div>
       <DeleteWorkoutButton workoutId={workout.id} />
       <EditWorkoutButton workoutId={workout.id} />
